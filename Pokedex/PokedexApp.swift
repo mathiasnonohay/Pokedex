441b4dc8ab9265f57489f475f270eaed2283a6ca
Mathias Nonohay <mathias.gianotti.nonohay@sibs.com>
2023-10-20 17:59:18 +0100

    Initial Commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Mathias Nonohay on 20/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
